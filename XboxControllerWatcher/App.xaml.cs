b461b3c18524dc34a457b458c241cbf1693d4650
jakolion <jakolion@gmail.com>
2019-04-20 17:50:47 +0200

    added mutex for single instance

--- XboxControllerWatcher/App.xaml.cs
+++ XboxControllerWatcher/App.xaml.cs
@@ -1,13 +1,28 @@
-﻿using System.Windows;
+﻿using System.Threading;
+using System.Windows;
 
 namespace XboxControllerWatcher
 {
     public partial class App : Application
     {
+        private static Mutex mutex;
+
         void AppStartup ( object sender, StartupEventArgs e )
         {
-            InfoWindow infoWindow = new InfoWindow();
-            _ = new Watcher( infoWindow );
+            // try to create mutex for checking for another instance
+            bool mutexCreatedSuccessfully;
+            mutex = new Mutex( true, System.Reflection.Assembly.GetExecutingAssembly().GetName().Name, out mutexCreatedSuccessfully );
+            if ( mutexCreatedSuccessfully )
+            {
+                // no other instance running
+                InfoWindow infoWindow = new InfoWindow();
+                _ = new Watcher( infoWindow );
+            }
+            else
+            {
+                // another instance is already running
+                Current.Shutdown();
+            }
         }
     }
 }
