d4aaa451813e74fa063447b8d0023bd139bdb337
jakolion <jakolion@gmail.com>
2019-04-28 00:32:36 +0200

    added fading animation to hotkey detection window

--- XboxControllerWatcher/Constants.cs
+++ XboxControllerWatcher/Constants.cs
@@ -6,9 +6,11 @@
         public const int POLL_INTERVAL_HOTKEY = 20;
         public const int POLL_INTERVAL_HOTKEY_INACTIVE = 1000;
         public const int POLL_INTERVAL_BATTERY_LEVEL = 1000;
-        public const int WINDOW_INFO_SHOW_DURATION = 5000;
-        public const int WINDOW_INFO_FADING_IN_DURATION = 1000;
-        public const int WINDOW_INFO_FADING_OUT_DURATION = 250;
         public const int WINDOW_HOTKEY_DETECTED_SHOW_DURATION = 3000;
+        public const int WINDOW_HOTKEY_DETECTED_FADE_IN_DURATION = 100;
+        public const int WINDOW_HOTKEY_DETECTED_FADE_OUT_DURATION = 100;
+        public const int WINDOW_INFO_SHOW_DURATION = 5000;
+        public const int WINDOW_INFO_FADE_IN_DURATION = 1000;
+        public const int WINDOW_INFO_FADE_OUT_DURATION = 250;
     }
 }
--- XboxControllerWatcher/WindowHotkeyDetection.xaml.cs
+++ XboxControllerWatcher/WindowHotkeyDetection.xaml.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Threading;
+using System.Windows.Media.Animation;
 
 namespace XboxControllerWatcher
 {
     public partial class WindowHotkeyDetection : Window
     {
-        DispatcherTimer _timer;
+        private DispatcherTimer _timer;
+        private DoubleAnimation _animation;
+
         public WindowHotkeyDetection ()
         {
             InitializeComponent();
+
+            // set opacity
+            Opacity = 0.0;
+            
+            // configure animation for fading
+            _animation = new DoubleAnimation();
+            _animation.BeginTime = null;
+            _animation.FillBehavior = FillBehavior.HoldEnd;
+            Timeline.SetDesiredFrameRate( _animation, 60 );
 
             // create timer
             _timer = new DispatcherTimer();
@@ -19,23 +31,78 @@ namespace XboxControllerWatcher
 
         public void ShowWindow ( string name )
         {
+            // stop the animation
+            if ( _animation.BeginTime != null )
+            {
+                _animation.BeginTime = null;
+                _animation.Completed -= FadingFinished;
+            }
+
             // stop the timer if running
             _timer.Stop();
 
             // set text
             text.Text = name;
 
-            // show
             Show();
 
-            // start the timer
-            _timer.Start();
+            // start auto hide timer if opacity is 100%
+            if ( Opacity == 1.0 )
+            {
+                _timer.Start();
+                return;
+            }
+
+            // start new fade in animation from current opacity to 100%
+            _animation.BeginTime = TimeSpan.FromMilliseconds( 0 );
+            _animation.From = Opacity;
+            _animation.To = 1.0;
+            int duration = Convert.ToInt32( Constants.WINDOW_HOTKEY_DETECTED_FADE_IN_DURATION * ( 1.0 - Opacity ) );
+            _animation.Duration = TimeSpan.FromMilliseconds( duration );
+            _animation.Completed += FadingFinished;
+            BeginAnimation( OpacityProperty, _animation );
+        }
+
+        private void FadeOut ()
+        {
+            // stop the animation
+            if ( _animation.BeginTime != null )
+            {
+                _animation.BeginTime = null;
+                _animation.Completed -= FadingFinished;
+            }
+            
+            // if opacity is already 0%, there is no need for an animation
+            if ( Opacity == 0.0 )
+            {
+                Hide();
+                return;
+            }
+
+            // start new fade out animation from current opacity to 0% or 50%
+            _animation.BeginTime = TimeSpan.FromMilliseconds( 0 );
+            _animation.From = Opacity;
+            _animation.To = 0.0;
+            int duration = Convert.ToInt32( Constants.WINDOW_HOTKEY_DETECTED_FADE_OUT_DURATION * ( Opacity - _animation.To ) );
+            _animation.Duration = TimeSpan.FromMilliseconds( duration );
+            _animation.Completed += FadingFinished;
+            BeginAnimation( OpacityProperty, _animation );
+        }
+
+        private void FadingFinished ( object sender, EventArgs e )
+        {
+            // stop animation
+            _animation.BeginTime = null;
+            _animation.Completed -= FadingFinished;
+
+            // start timer if opacity is 100%
+            if ( _animation.To == 1.0 )
+                    _timer.Start();
         }
 
         private void OnTimerEvent ( object sender, EventArgs e )
         {
-            // hide the window
-            Hide();
+            FadeOut();
         }
 
         private void Window_SizeChanged ( object sender, SizeChangedEventArgs e )
