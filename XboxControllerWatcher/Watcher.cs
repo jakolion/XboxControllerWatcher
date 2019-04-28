5531175ad2bdb08c245e2c11b933530e3f80ceaf
jakolion <jakolion@gmail.com>
2019-04-28 21:58:36 +0200

    catch exceptions of the watcher class

--- XboxControllerWatcher/Watcher.cs
+++ XboxControllerWatcher/Watcher.cs
@@ -41,102 +41,108 @@ namespace XboxControllerWatcher
 
         private void OnHotkeyTimerEvent ( object source, EventArgs e )
         {
-            // check if there is at least one controller connected
-            bool isAnyControllerConnected = false;
-            for ( uint i = 0; i < Constants.MAX_CONTROLLERS; i++ )
+            // this try/catch ensures that the loop doesn't stop in case of an exception
+            try
             {
-                if ( _controllerList[i].isConnected )
+                // check if there is at least one controller connected
+                bool isAnyControllerConnected = false;
+                for ( uint i = 0; i < Constants.MAX_CONTROLLERS; i++ )
                 {
-                    isAnyControllerConnected = true;
-                    break;
-                }
-            }
-
-            // check if there are any hotkeys to be monitored
-            bool isAnyHotkeyEnabled = false;
-            if ( isAnyControllerConnected )
-            {
-                foreach ( Hotkey hotkey in _main.settings.hotkeys )
-                {
-                    if ( hotkey.isEnabled )
+                    if ( _controllerList[i].isConnected )
                     {
-                        isAnyHotkeyEnabled = true;
+                        isAnyControllerConnected = true;
                         break;
                     }
                 }
-            }
 
-            // check if at least one controller is connected and one
-            // hotkey is defined, otherwise we want to reduce the load
-            if ( isAnyControllerConnected && isAnyHotkeyEnabled )
-            {
-                // set short intervall and continue
-                _hotkeyTimer.Interval = Constants.POLL_INTERVAL_HOTKEY;
-            }
-            else
-            {
-                // set long interval and restart the loop
-                _hotkeyTimer.Interval = Constants.POLL_INTERVAL_HOTKEY_INACTIVE;
-
-                // restart timer
-                _hotkeyTimer.Start();
-
-                return;
-            }
-
-            // check all controllers for button presses
-            for ( uint i = 0; i < Constants.MAX_CONTROLLERS; i++ )
-            {
-                // get current button state
-                ushort state = XInput.GetButtonState( i );
-
-                // loop through hotkeys
-                bool hotkeyDetected = false;
-                bool hotkeyDetectedWindowShown = false;
-                foreach ( Hotkey hotkey in _main.settings.hotkeys )
+                // check if there are any hotkeys to be monitored
+                bool isAnyHotkeyEnabled = false;
+                if ( isAnyControllerConnected )
                 {
-                    if ( hotkey.isEnabled )
+                    foreach ( Hotkey hotkey in _main.settings.hotkeys )
                     {
-                        if ( hotkey.buttonState.ButtonsToNum() == state )
+                        if ( hotkey.isEnabled )
                         {
-                            // hotkey detected
-                            hotkeyDetected = true;
-
-                            // execute command
-                            if ( hotkey.isCommandBatteryLevel )
-                            {
-                                if ( _controllerList[i].isConnected )
-                                    _main.windowInfo.ShowInfo( "Controller " + ( i + 1 ) + " Battery Level", _controllerList[i] );
-                            }
-                            else
-                            {
-                                // show detected window
-                                if ( !hotkeyDetectedWindowShown )
-                                {
-                                    _main.ShowHotkeyDetected( hotkey.name );
-                                    hotkeyDetectedWindowShown = true;
-                                }
-
-                                // start the process
-                                Process process = new Process();
-                                process.StartInfo.FileName = "cmd";
-                                process.StartInfo.Arguments = "/c " + hotkey.commandCustom;
-                                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                                process.Start();
-                            }
+                            isAnyHotkeyEnabled = true;
+                            break;
                         }
                     }
                 }
 
-                // if hotkey detected, wait for release
-                if ( hotkeyDetected )
+                // check if at least one controller is connected and one
+                // hotkey is defined, otherwise we want to reduce the load
+                if ( isAnyControllerConnected && isAnyHotkeyEnabled )
                 {
-                    while ( state == XInput.GetButtonState( i ) )
+                    // set short intervall and continue
+                    _hotkeyTimer.Interval = Constants.POLL_INTERVAL_HOTKEY;
+                }
+                else
+                {
+                    // set long interval and restart the loop
+                    _hotkeyTimer.Interval = Constants.POLL_INTERVAL_HOTKEY_INACTIVE;
+
+                    // restart timer
+                    _hotkeyTimer.Start();
+
+                    return;
+                }
+
+                // check all controllers for button presses
+                for ( uint i = 0; i < Constants.MAX_CONTROLLERS; i++ )
+                {
+                    // get current button state
+                    ushort state = XInput.GetButtonState( i );
+
+                    // loop through hotkeys
+                    bool hotkeyDetected = false;
+                    bool hotkeyDetectedWindowShown = false;
+                    foreach ( Hotkey hotkey in _main.settings.hotkeys )
                     {
-                        Thread.Sleep( Constants.POLL_INTERVAL_HOTKEY );
+                        if ( hotkey.isEnabled )
+                        {
+                            if ( hotkey.buttonState.ButtonsToNum() == state )
+                            {
+                                // hotkey detected
+                                hotkeyDetected = true;
+
+                                // execute command
+                                if ( hotkey.isCommandBatteryLevel )
+                                {
+                                    if ( _controllerList[i].isConnected )
+                                        _main.windowInfo.ShowInfo( "Controller " + ( i + 1 ) + " Battery Level", _controllerList[i] );
+                                }
+                                else
+                                {
+                                    // show detected window
+                                    if ( !hotkeyDetectedWindowShown )
+                                    {
+                                        _main.ShowHotkeyDetected( hotkey.name );
+                                        hotkeyDetectedWindowShown = true;
+                                    }
+
+                                    // start the process
+                                    Process process = new Process();
+                                    process.StartInfo.FileName = "cmd";
+                                    process.StartInfo.Arguments = "/c " + hotkey.commandCustom;
+                                    process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                                    process.Start();
+                                }
+                            }
+                        }
+                    }
+
+                    // if hotkey detected, wait for release
+                    if ( hotkeyDetected )
+                    {
+                        while ( state == XInput.GetButtonState( i ) )
+                        {
+                            Thread.Sleep( Constants.POLL_INTERVAL_HOTKEY );
+                        }
                     }
                 }
+
             }
+            catch { }
 
             // restart timer
             _hotkeyTimer.Start();
@@ -144,47 +150,53 @@ namespace XboxControllerWatcher
 
         private void OnBatteryTimerEvent ( object source, ElapsedEventArgs e )
         {
-            // check all controllers for new status
-            for ( uint i = 0; i < Constants.MAX_CONTROLLERS; i++ )
+            // this try/catch ensures that the loop doesn't stop in case of an exception
+            try
             {
-                // get current status
-                Controller newController = XInput.GetBatteryInformation( i );
-                Controller oldController = _controllerList[i];
-
-                // check for update
-                if ( newController.isConnected != oldController.isConnected || newController.batteryLevel != oldController.batteryLevel )
+                // check all controllers for new status
+                for ( uint i = 0; i < Constants.MAX_CONTROLLERS; i++ )
                 {
-                    // update controller list
-                    _controllerList[i] = newController;
+                    // get current status
+                    Controller newController = XInput.GetBatteryInformation( i );
+                    Controller oldController = _controllerList[i];
 
-                    // compare status
-                    if ( newController.isConnected != oldController.isConnected )
+                    // check for update
+                    if ( newController.isConnected != oldController.isConnected || newController.batteryLevel != oldController.batteryLevel )
                     {
-                        // connection status has been changed
-                        if ( newController.isConnected )
+                        // update controller list
+                        _controllerList[i] = newController;
+
+                        // compare status
+                        if ( newController.isConnected != oldController.isConnected )
                         {
-                            // controller has been connected
-                            _main.windowInfo.ShowInfo( "Controller " + ( i + 1 ) + " Battery Level (connected)", newController );
+                            // connection status has been changed
+                            if ( newController.isConnected )
+                            {
+                                // controller has been connected
+                                _main.windowInfo.ShowInfo( "Controller " + ( i + 1 ) + " Battery Level (connected)", newController );
+                            }
+                            else
+                            {
+                                // controller has been disconnected
+                                // show last battery level
+                                Controller disconnectedController = oldController;
+                                disconnectedController.isConnected = false;
+                                _main.windowInfo.ShowInfo( "Controller " + ( i + 1 ) + " Battery Level (disconnected)", disconnectedController );
+                            }
                         }
-                        else
+                        else if ( newController.batteryLevel != oldController.batteryLevel )
                         {
-                            // controller has been disconnected
-                            // show last battery level
-                            Controller disconnectedController = oldController;
-                            disconnectedController.isConnected = false;
-                            _main.windowInfo.ShowInfo( "Controller " + ( i + 1 ) + " Battery Level (disconnected)", disconnectedController );
+                            // battery level has changed
+                            _main.windowInfo.ShowInfo( "Controller " + ( i + 1 ) + " Battery Level", newController );
                         }
-                    }
-                    else if ( newController.batteryLevel != oldController.batteryLevel )
-                    {
-                        // battery level has changed
-                        _main.windowInfo.ShowInfo( "Controller " + ( i + 1 ) + " Battery Level", newController );
                     }
                 }
-            }
 
-            // update context menu
-            _main.SetBatteryLevelMenuItems( _controllerList );
+                // update context menu
+                _main.SetBatteryLevelMenuItems( _controllerList );
+
+            }
+            catch { }
 
             // restart timer
             _batteryTimer.Start();
